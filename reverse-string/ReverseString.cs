87be5c78cadaefa723f9a47017601011b5b1fef6
Akshive Pandey <pandeyakshive97@gmail.com>
2019-06-12 11:59:55 +0530

    Add files via upload

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if(input == null)return null;
+        if(string.Compare(input, "") == 0)return "";
+        char[] temp = input.ToCharArray();
+        Array.Reverse(temp);
+        return new string(temp);
+    }
+}
---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,11 @@
+﻿public static class TwoFer
+{
+    public static string Speak()
+    {
+        return "One for you, one for me."
+;    }
+    public static string Speak(string a)
+    {
+        return $"One for {a}, one for me.";
+    }
+}
